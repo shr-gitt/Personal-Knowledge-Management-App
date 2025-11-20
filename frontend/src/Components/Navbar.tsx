5f47d917c5580dbec0d0bafd2115de1dc016c2e9
shr-gitt <134399390+shr-gitt@users.noreply.github.com>
2025-11-20 13:42:48 +0545

    NavBar

--- frontend/src/Components/Navbar.tsx
+++ frontend/src/Components/Navbar.tsx
@@ -1,11 +1,20 @@
 import { Link } from 'react-router-dom';
+import { FaHome, FaSearch, FaPlus, FaChartLine, FaTrash, FaCog, FaUser } from "react-icons/fa";
+import "./Navbar.css";
 
 function Navbar() {
     return (
-    <nav>
-        <Link to="/">Home</Link> | 
-        <Link to="/login">Login</Link> | 
-        <Link to="/notes">Notes</Link>
+    <nav className="sidebar">
+        <Link to="/" className='item'><FaHome /><span>Home</span></Link>
+        <Link to="/search" className='item'><FaSearch /><span>Search</span></Link>
+        <Link to="/create_notes" className='item'><FaPlus /><span>Create Notes</span></Link>
+        <Link to ="/graph_view" className='item'><FaChartLine /><span>Graph View</span></Link>
+        <Link to="/trash" className='item'><FaTrash /><span>Trash</span></Link>
+        
+        <div className='bottom'>
+            <Link to="/settings" className='item'><FaCog /><span>Settings</span></Link>
+            <Link to="/profile" className='item'><FaUser /><span>Profile</span></Link>
+        </div> 
     </nav>
     );
 }
