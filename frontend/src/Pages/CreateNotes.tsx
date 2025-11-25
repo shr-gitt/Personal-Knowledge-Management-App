8ef180643b88c73f19ca6525f65b703bef840ff9
shr-gitt <134399390+shr-gitt@users.noreply.github.com>
2025-11-25 18:21:29 +0545

    Text Notes

--- frontend/src/Pages/CreateNotes.tsx
+++ frontend/src/Pages/CreateNotes.tsx
@@ -1,5 +1,28 @@
+import { useState } from "react"
+import Button from "../Components/Button";
+import { useNavigate } from "react-router-dom";
+import './CreateNotes.css'
+
 const CreateNotes = () =>{
-    return  <h1>CreateNotes Page</h1>
+    const [text, setText] = useState("");
+
+    const title = text.split("\n")[0];
+    const body = text.split("\n").slice(1).join("\n");
+
+    const navigate = useNavigate();
+
+    return(<div className="page">
+        <textarea
+            value = {text}
+            onChange={(e)=>setText(e.target.value)}
+            rows={10}
+            className="noteBox"
+            placeholder="Write new note here."
+        />
+        <div className="saveButton">
+            <Button onClick={() => navigate("/")} color="primary">Save</Button>
+        </div>
+    </div>)
 }
 
 export default CreateNotes
