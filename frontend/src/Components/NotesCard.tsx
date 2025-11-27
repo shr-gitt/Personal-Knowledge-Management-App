f6218ff6ced4df4760a299529fbb94d2875669dd
shr-gitt <134399390+shr-gitt@users.noreply.github.com>
2025-11-27 12:53:21 +0545

    Notes Card

---
+++ frontend/src/Components/NotesCard.tsx
@@ -0,0 +1,28 @@
+import "./NotesCard.css"
+
+interface Note {
+    id: number;
+    title: string;
+    content: string;
+    updatedAt: string;
+}
+
+interface Props {
+    notes: Note[];
+}
+
+const NotesCard= ({notes}:Props)=>{
+    return <div>
+        {notes.map((note) =>(
+            <div key={note.id} className="note-card">
+                <div className="title-bar">
+                    <h5 className="title">{note.title}</h5>
+                    <span className="dates">{note.updatedAt}</span>
+                </div>
+                <p className="information">{note.content}</p>
+            </div>
+        ))}
+    </div>
+}
+
+export default NotesCard;
