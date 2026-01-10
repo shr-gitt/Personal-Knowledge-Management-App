82379e599fab66731d2e35c5d5a21878fce4af86
shr-gitt <134399390+shr-gitt@users.noreply.github.com>
2026-01-10 09:54:53 +0545

    Backend in JAVA initialized

---
+++ PKM-Backend-JAVA/src/main/java/com/example/PKM_Backend_JAVA/PkmBackendJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.PKM_Backend_JAVA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkmBackendJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PkmBackendJavaApplication.class, args);
+	}
+
+}
---
+++ PKM-Backend-JAVA/src/test/java/com/example/PKM_Backend_JAVA/PkmBackendJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PKM_Backend_JAVA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PkmBackendJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
